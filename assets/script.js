ea6052da6ca34e02cf31b8855c051aa79b25d4a6
DimitriNakos <dimi1nakos@gmail.com>
2020-11-04 18:48:28 -0500

    building the basic structure up.  adding testing JS

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var questionSect = document.getElementById("questionSect");
+var answersSect = document.getElementById("answersSect");
+var rightWrong = document.getElementById("rightWrong");
+
+
+
+
+var questObjArr = [
+    {
+        question: "What is JavaScript?",
+        answers: ["A fancier version of Java that uses scripts.", "An Object oriented programming language.", "A replacement for HTML and CSS.",],
+        correctAnswer: 1
+    }
+]
+
+
