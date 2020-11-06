8b34720fb826a4aa27949aaf718ee782862066ed
DimitriNakos <dimi1nakos@gmail.com>
2020-11-05 20:46:51 -0500

    lots of logic and timers created and new function

--- assets/script.js
+++ assets/script.js
@@ -7,39 +7,7 @@ var countDownQuizId = document.getElementById("countDownQuizId")
 var countDownId = document.getElementById("countDownId")
 var listParent = document.querySelector("#listParent")
 
-var secondsRemainQuiz = 600
-var secondsRemainQuestion = 60
-
-var correctA = 0
-var incorrectA = 0
-
-function quizTimer() {
-    var quizInterval = setInterval(function () {
-        secondsRemainQuiz--;
-        countDownQuizId.textContent = secondsRemainQuiz;
-
-        if (secondsRemainQuiz === 0) {
-            clearInterval(quizInterval)
-            questionSect.textContent = "DID NOT FINISH IN TIME!"
-            rightWrong.textContent = "DID NOT FINISH IN TIME!"
-            //ADD MORE CONTENT
-        }
-    }, 1000)
-}
-quizTimer();
-
-function questionTimer() {
-    var questionInterval = setInterval(function () {
-        secondsRemainQuestion--;
-        countDownId.textContent = secondsRemainQuestion;
-
-        if (secondsRemainQuestion === 0) {
-            clearInterval(questionInterval);
-            // FILL IN CONTENT HERE
-        }
-    }, 1000)
-}
-questionTimer();
+var secondsRemainQuiz = 100
 
 var questObjArr = [
     {
@@ -107,18 +75,98 @@ var questObjArr = [
     },
 ]
 
-//  ERASE AFTER RUNNING CORRECTLY.  THIS IS FOR TESTING.
-function clicker() {
+var questionNum = 0
+var quizInterval
 
-    rightWrong.textContent = "TESTING TESTING";
-
-    for (i = 0; i < questObjArr[i].answers.length; ++i) {
-        questionSect.textContent = questObjArr[i].question
-        listItem = document.createElement('button');
-        listItem.innerHTML = questObjArr[i].answers[i];
-        listParent.appendChild(listItem);
-    }
-
+function quizTimer() {
+    quizInterval = setInterval(function () {
+        secondsRemainQuiz--;
+        countDownQuizId.textContent = secondsRemainQuiz;
+        if (secondsRemainQuiz === 0) {
+            outOfTime();
+        } else if (questionNum === questObjArr.length - 1) {
+            clearInterval(quizInterval)
+            endGame();
+        }
+    }, 1000)
 }
 
-testBtn.addEventListener("click", clicker);
+function clicker() {
+    rightWrong.textContent = "TESTING TESTING";
+    questionSect.textContent = questObjArr[questionNum].question
+    listParent.innerHTML = "";
+
+    for (i = 0; i < questObjArr[questionNum].answers.length; ++i) {
+        listItem = document.createElement('button');
+        listItem.innerHTML = questObjArr[questionNum].answers[i];
+        listItem.setAttribute("id", i)
+        listParent.appendChild(listItem);
+    }
+}
+
+function startGame() {
+    clicker();
+    quizTimer();
+}
+testBtn.addEventListener("click", startGame);
+
+function answerInput(event) {
+    if (event.target.matches("button")) {
+        event.preventDefault();
+        console.log(event.target.id)
+
+        if (questObjArr[questionNum].correctAnswer !== parseInt(event.target.id)) {
+            secondsRemainQuiz -= 10;
+        }
+        if (questObjArr.length - 1 !== questionNum) {
+            questionNum++;
+            clicker();
+        } else {
+            endGame();
+        }
+    }
+}
+listParent.addEventListener("click", answerInput)
+
+//  Game Ends
+function endGame() {
+    //  clear HTML to show different content
+    questionSect.textContent = "Congrats! You had " + secondsRemainQuiz + " seconds left!"
+    rightWrong.textContent = "Congrats! You had " + secondsRemainQuiz + " seconds left!"
+    listParent.prepend(listItem);
+    listParent.textContent = "Congrats! You had " + secondsRemainQuiz + " seconds left!"
+}
+//  Stop the timer
+function outOfTime() {
+    clearInterval(quizInterval)
+    questionSect.textContent = "DID NOT FINISH IN TIME!"
+    rightWrong.textContent = "DID NOT FINISH IN TIME!"
+    listParent.prepend(listItem);
+    listParent.textContent = "DID NOT FINISH IN TIME!  You don't even qualify for a score."
+}
+
+//  have a form for users initials
+//  submit button to save initials to variable for local storage
+//  store the data as an objects
+//  read from local storage and convert local storage JSON  to store
+//  push object into an array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
